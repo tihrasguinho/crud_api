46a39c610e3a3a232dd25367e123270d2aa929e9
Tiago Alves <tiago.pessoal93@gmail.com>
2021-10-24 16:04:03 -0300

    SUPABASE ENVIROMENT FIX

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:supabase/supabase.dart';
 
 class SupabaseService {
   static final _client = SupabaseClient(
-    String.fromEnvironment('SUPABASE_URL'),
-    String.fromEnvironment('SUPABASE_KEY'),
+    Platform.environment['SUPABASE_URL']!,
+    Platform.environment['SUPABASE_KEY']!,
   );
 
   SupabaseClient get client => _client;

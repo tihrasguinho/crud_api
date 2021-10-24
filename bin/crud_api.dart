8b13de03decad8df946c5a493142874ca08a58cd
Tiago Alves <tiago.pessoal93@gmail.com>
2021-10-24 15:54:56 -0300

    PORT FIX

--- bin/crud_api.dart
+++ bin/crud_api.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:crud_api/app_module.dart';
 import 'package:shelf_modular/shelf_modular.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
+  final port = Platform.environment['PORT'];
+
   final server = await io.serve(
     Modular(module: AppModule()),
     '0.0.0.0',
-    int.fromEnvironment('PORT'),
+    int.tryParse(port!) ?? 8080,
   );
   print('Server started: ${server.address.address}:${server.port}');
 }
